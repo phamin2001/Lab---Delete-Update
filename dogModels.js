079b526fae3adb3bd82b6fd74ba8e45feff5054b
phamin2001 <amin.pahlavani@gmail.com>
2019-01-16 10:39:30 -0700

    commit

---
+++ dogModels.js
@@ -0,0 +1,16 @@
+const dog = [
+    {
+        name: "sheppird",
+        age: 5
+    } ,
+    {
+        name: "puppi",
+        age: 3
+    } ,
+    {
+        name: "lolu",
+        age: 6
+    }
+]
+
+module.exports = dog;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require ('express');
 const app = express();
+const Dog = require('./dogModel');
 
+//
 
 
 
